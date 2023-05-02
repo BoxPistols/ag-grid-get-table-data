0c44b2e6812f499ab4fe6d293e80d38f33dc3563
aito <BoxPistols@users.noreply.github.com>
2023-05-03 05:16:41 +0900

    get json tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react"
 import "ag-grid-enterprise"
-// import { Divider } from "@mui/material";
 import Grid from "./Grid"
 import "./styles.css"
 import SideItems from "./SideItems"
 import Box from "@mui/material/Box"
 
 export default function App() {
-  const [selectedItem, setSelectedItem] = useState<number | null>(null)
+  const [selectedItem, setSelectedItem] = useState<any[] | null | string>(null) // <- 変更
+  const [, setKeyList] = useState<string[]>([])
 
   return (
     <>
       <h2>AgGrid Example</h2>
       <Box display="flex">
-        <SideItems onItemClick={(index) => setSelectedItem(index)} />
-        <Grid selectedItem={selectedItem} />
+        <SideItems onItemClick={(data) => setSelectedItem(data)} />
+        <Grid selectedItem={selectedItem} onKeysUpdate={setKeyList} />
       </Box>
     </>
   )
