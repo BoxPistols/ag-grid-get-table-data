1514bc62a5e05154d58868044a7daffa50f11d48
aito <BoxPistols@users.noreply.github.com>
2023-05-08 05:33:14 +0900

    Fake Data

--- src/components/ColumnLists.tsx
+++ src/components/ColumnLists.tsx
@@ -10,16 +10,47 @@ type ColumnListsProps = {
 
 const ColumnLists: React.FC<ColumnListsProps> = ({ keyList, title,sx }) => {
   return (
-    <Box border={"1px solid #eee"} px={1} py={2} sx={sx}>
-      <Typography variant="body2" gutterBottom>
+    <Box
+      border={"1px solid #eee"}
+      sx={sx}
+      style={{
+        maxHeight: 600,
+        overflow: "auto",
+        position: "relative",
+      }}
+      >
+      <Typography
+        fontSize={16}
+        position="sticky"
+        top={0}
+        py={1}
+        sx={{
+          backgroundColor: "#f9f9f9",
+          borderBottom: "1px solid #eee",
+          py: 1,
+          px: 2,
+          margin: 0,
+        }}
+      >
         {title}
       </Typography>
       <ul>
         {keyList.map((key, index) => (
-          <li key={index}>{key}</li> // ここのListはTableのColumn名の一覧になる
+          <li key={index} style={{fontSize: 14}}>{key}</li> // ここのListはTableのColumn名の一覧になる
         ))}
       </ul>
-      <Typography variant="subtitle1">
+      <Typography
+        variant="subtitle1"
+        position="sticky"
+        bottom={0}
+        sx={{
+          backgroundColor: "#f9f9f9",
+          borderTop: "1px solid #eee",
+          py: 1,
+          px: 2,
+          margin: 0,
+        }}
+      >
         Total Columns: {keyList.length}
       </Typography>
     </Box>
